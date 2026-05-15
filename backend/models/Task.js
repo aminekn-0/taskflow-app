8ae8bdcf63d78d024c429b34dcf10706494b6709
ilham-Auer <ilhamauer@gmail.com>
2026-05-15 20:56:55 +0100

    refactor : fix quotes in task model and routes

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,41 +1,41 @@
-const mongoose = require(‘mongoose’);
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema(
-{
-title: {
-type: String,
-required: [true, ‘Le titre est obligatoire’],
-trim: true,
-},
-description: {
-type: String,
-trim: true,
-},
-priority: {
-type: String,
-enum: [‘low’, ‘medium’, ‘hight’],
-required: [true, ‘La priorité est obligatoire’],
-},
-status: {
-type: String,
-enum: [‘todo’, ‘in progress’, ‘done’],
-default: ‘todo’,
-},
-dueDate: {
-type: Date,
-},
-project: {
-type: mongoose.Schema.Types.ObjectId,
-ref: ‘Project’,
-required: [true, ‘Le projet parent est obligatoire’],
-},
-assignedTo: {
-type: mongoose.Schema.Types.ObjectId,
-ref: ‘User’,
-default: null,
-},
-},
-{ timestamps: true }
+    {
+        title: {
+            type: String,
+            required: [true, "Le titre est obligatoire"],
+            trim: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "hight"],
+            required: [true, "La priorité est obligatoire"],
+        },
+        status: {
+            type: String,
+            enum: ["todo", "in progress", "done"],
+            default: "todo",
+        },
+        dueDate: {
+            type: Date,
+        },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: [true, "Le projet parent est obligatoire"],
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: null,
+        },
+    },
+    { timestamps: true }
 );
 
-module.exports = mongoose.model(‘Task’, taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
