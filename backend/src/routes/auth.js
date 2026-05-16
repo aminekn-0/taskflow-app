26482e69a914e0570bc24cb9241e450d2894a6ac
Oanekrif <130631445+Oanekrif@users.noreply.github.com>
2026-05-17 00:01:44 +0100

    feat: fix the structure of files

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middlewares/auth');
 
 // Helper function to generate JWT token
 const generateToken = (id) => {
