c2bcf56639c445b965a884f44dd63ac96bcdba38
rawya <raouiamokhchan2005@gmail.com>
2026-05-17 17:19:43 +0000

    changes in the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const projectRoutes = require("./routes/project.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // routes
