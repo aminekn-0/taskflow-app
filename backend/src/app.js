64f438ee73581ca279098b0904388b261621dff1
aminekn-0 <aminekn16@gmail.com>
2026-05-18 23:37:09 +0200

    feat : Add activity and notification routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const projectRoutes = require("./routes/project.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
 
+const activityRoutes = require("./routes/activityRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+
 const app = express();
 
 app.use(cors({ origin: "*" }));
@@ -12,5 +15,7 @@ app.use(express.json());
 // routes
 app.use("/api/projects", projectRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/projects/:id/activities", activityRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 module.exports = app;
